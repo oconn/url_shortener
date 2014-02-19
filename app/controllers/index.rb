c133d0b0b44a456a1ed756bd2bc8b3581c425fac
wisconsinpoet <dandydanny@gmail.com>
2014-02-19 09:01:12 -0600

    add new url logic

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,6 +8,12 @@ get '/:short_url' do
 end
 
 post '/urls' do
+  if Url.where(url: "http://google.com").length == 0
+    @short = gen_short_url
+  else
+    @short = Url.where(url: "http://www.google.com").first.short_url
+  end
+
   short_url = Url.gen_short_url
   # create a new Url
   # erb :index
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,5 @@
 class Url < ActiveRecord::Base
-  validates_format_of :url, :with => URI::regexp(%w(http https)), presence: true
+  validates_format_of :url, :with => URI::regexp(%w(http https)), presence: true, uniqueness: true
   validates :short_url, uniqueness: true, presence: true
 
   def self.gen_short_url
